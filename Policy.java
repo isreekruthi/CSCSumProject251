826e80b11a8dd763f5f3636b9ec81b794f7fa83a
Sree <isreekruthi@gmail.com>
2024-06-03 22:07:47 -0400

    Policy Class
    Getters and Setters etc

--- Policy.java
+++ Policy.java
@@ -1,3 +1,163 @@
-{
-   hi
+public class Policy
+{  
+   private String policyNumber;
+   private String providerName;
+   private String FirstName;
+   private String LastName;
+   private int age;
+   private String smokingStatus;
+   private double height;
+   private double weight;
+
+   public Policy()
+   {
+      policyNumber = "";
+      providerName = "";
+      FirstName = "";
+      LastName = "";
+      age = 0;
+      smokingStatus = "";
+      height = 0;
+      weight = 0;
+   }
+   
+   
+   public Policy(String policyNumber, String policyName, String firstName, String lastName,int a, String smokeStatus, double h, double w)
+   {
+      policyNumber = policyNumber;
+      providerName = policyName;
+      FirstName = firstName;
+      LastName = lastName;
+      age = a;
+      smokingStatus = smokeStatus;
+      height = h;
+      weight = w;
+   }
+   
+  
+   public String getPolicyNumber()
+   {
+      return policyNumber;
+   }
+   
+   
+   public String getProviderName()
+   {
+      return providerName;
+   }
+   
+   
+   public String getFirstName()
+   {
+      return FirstName;
+   }
+   
+   
+   public String getLastName()
+   {
+      return LastName;
+   }
+   
+   
+   public int getAge()
+   {
+      return age;
+   }
+   
+   
+   public String getSmokingStatus()
+   {
+      return smokingStatus;
+   }
+   
+   
+   public double getHeight()
+   {
+      return height;
+   }
+   
+   
+   public double getWeight()
+   {
+      return weight;
+   }
+   
+   
+   public void setPolicyNumber(String policyNumber)
+   {
+      policyNumber = policyNumber;
+   }
+   
+
+   public void setProviderName(String policyName)
+   {
+      providerName = policyName;
+   }
+   
+   
+   public void setFirstName(String firstName)
+   {
+      FirstName = firstName;
+   }
+   
+   
+   public void setLastName(String lastName)
+   {
+      LastName = lastName;
+   }
+   
+   public void setAge(int a)
+   {
+      age = a;
+   }
+   
+   
+   public void setSmokingStatus(String smokeStatus)
+   {
+      smokingStatus = smokeStatus;
+   }
+   
+   
+   public void setHeight(double h)
+   {
+      height = h;
+   }
+   
+      public void setWeight(double w)
+   {
+      weight = w;
+   }
+   
+   
+   public double getBMI()
+   {
+      final double CONVFACTOR = 703;
+      
+      return (weight * CONVFACTOR) / (height * height);
+   }
+   
+     public double getPrice()
+   {
+      final double BASE_PRICE = 600;
+      final double ADDITIONAL_FEE_AGE = 75;
+      final double ADDITIONAL_FEE_SMOKING = 100;
+      final double ADDITIONAL_FEE_PER_BMI = 20;
+      
+      final int AGE_THRESHOLD = 50;
+      final int BMI_THRESHOLD = 35;
+      
+      double price = BASE_PRICE;
+      
+      if(age > AGE_THRESHOLD)
+         price += ADDITIONAL_FEE_AGE;
+         
+      if(smokingStatus.equalsIgnoreCase("smoker"))
+         price += ADDITIONAL_FEE_SMOKING;
+      
+      if(getBMI() > BMI_THRESHOLD)
+         price += ((getBMI() - BMI_THRESHOLD) * ADDITIONAL_FEE_PER_BMI);
+         
+      return price;
+   }
+
 }
--- Project_sree_inuganti.java
+++ Project_sree_inuganti.java
@@ -1,3 +1,74 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+
+public class Project_sree_inuganti
 {
-   hi
+   public static void main(String[] args) throws IOException
+   {
+      
+      String policyNumber;
+      String providerName;
+      String firstName;
+      String lastName;
+      int age;
+      String smokingStatus;
+      double height;
+      double weight;
+      int numSmokers = 0;
+      
+      ArrayList<Policy> policyList = new ArrayList<Policy>();
+      
+      
+      File file = new File("PolicyInformation.txt");
+      Scanner inputFile = new Scanner(file);
+      
+      
+      while(inputFile.hasNext())
+      {
+      
+         policyNumber = inputFile.nextLine();
+         providerName = inputFile.nextLine();
+         firstName = inputFile.nextLine();
+         lastName = inputFile.nextLine();
+         age = inputFile.nextInt();
+         inputFile.nextLine();
+         smokingStatus = inputFile.nextLine();
+         height = inputFile.nextDouble();
+         weight = inputFile.nextDouble();
+         
+         
+         if(inputFile.hasNext())
+            inputFile.nextLine();
+         if(inputFile.hasNext())
+            inputFile.nextLine();
+            
+          policyList.add(new Policy(policyNumber, providerName, firstName, lastName, age, smokingStatus, height, weight));
+      
+      }
+      
+      
+      for(Policy policy : policyList)
+      { 
+         System.out.println("Policy Number: " + policy.getPolicyNumber());
+         System.out.println("Provider Name: " + policy.getProviderName());
+         System.out.println("Policyholder's First Name: " + policy.getFirstName());
+         System.out.println("Policyholder's Last Name: " + policy.getLastName());
+         System.out.println("Policyholder's Age: " + policy.getAge());
+         System.out.println("Policyholder's Smoking Status: " + policy.getSmokingStatus());
+         System.out.println("Policyholder's Height: " + policy.getHeight() + " inches");
+         System.out.println("Policyholder's Weight: " + policy.getWeight() + " pounds");
+         System.out.printf("Policyholder's BMI: %.2f\n", policy.getBMI());
+         System.out.printf("Policy Price: $%.2f\n", policy.getPrice());
+         System.out.println();
+         
+         if(policy.getSmokingStatus().equalsIgnoreCase("smoker"))
+            numSmokers++;
+      }
+      
+      
+      System.out.println("The number of policies with a smoker is: " + numSmokers);
+      System.out.println("The number of policies with a non-smoker is: " + (policyList.size() - numSmokers) );
+   }
 }
