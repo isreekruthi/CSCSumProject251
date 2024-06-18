725cf55011bd886a51fb93f2b23c1ad28b7bdbe7
Sree <isreekruthi@gmail.com>
2024-06-17 22:11:57 -0400

    Update Project_sree_inuganti.java
    Added documentation. Updated for Project 2

--- Project_sree_inuganti.java
+++ Project_sree_inuganti.java
@@ -3,11 +3,14 @@ import java.util.ArrayList;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * This class reads policy information from a text file, calculates BMI and policy price,
+ * and outputs policy details along with statistics about smokers and non-smokers.
+ */
 public class Project_sree_inuganti
 {
    public static void main(String[] args) throws IOException
    {
-      
       String policyNumber;
       String providerName;
       String firstName;
@@ -20,35 +23,34 @@ public class Project_sree_inuganti
       
       ArrayList<Policy> policyList = new ArrayList<Policy>();
       
-      
+      // Read data from the text file
       File file = new File("PolicyInformation.txt");
       Scanner inputFile = new Scanner(file);
       
-      
+      // Process each set of policy information in the file
       while(inputFile.hasNext())
       {
-      
          policyNumber = inputFile.nextLine();
          providerName = inputFile.nextLine();
          firstName = inputFile.nextLine();
          lastName = inputFile.nextLine();
          age = inputFile.nextInt();
-         inputFile.nextLine();
+         inputFile.nextLine(); // Consume newline left by nextInt()
          smokingStatus = inputFile.nextLine();
          height = inputFile.nextDouble();
          weight = inputFile.nextDouble();
          
-         
+         // Consume extra newlines if present
          if(inputFile.hasNext())
             inputFile.nextLine();
          if(inputFile.hasNext())
             inputFile.nextLine();
             
-          policyList.add(new Policy(policyNumber, providerName, firstName, lastName, age, smokingStatus, height, weight));
-      
+         // Create Policy object and add to the list
+         policyList.add(new Policy(policyNumber, providerName, firstName, lastName, age, smokingStatus, height, weight));
       }
       
-      
+      // Output details of each policy
       for(Policy policy : policyList)
       { 
          System.out.println("Policy Number: " + policy.getPolicyNumber());
@@ -63,11 +65,12 @@ public class Project_sree_inuganti
          System.out.printf("Policy Price: $%.2f\n", policy.getPrice());
          System.out.println();
          
+         // Count number of smokers
          if(policy.getSmokingStatus().equalsIgnoreCase("smoker"))
             numSmokers++;
       }
       
-      
+      // Output statistics
       System.out.println("The number of policies with a smoker is: " + numSmokers);
       System.out.println("The number of policies with a non-smoker is: " + (policyList.size() - numSmokers) );
    }
